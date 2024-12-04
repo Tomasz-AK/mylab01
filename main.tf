6f0de2c0ee318020a23092971a7fd5948dc612f0
tomasz <t.szymanski94@gmail.com>
2024-12-04 15:22:44 +0100

    Adding variables

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 resource "digitalocean_vpc" "main" {
-    name = "main-piotrkoska-tf-cloud"
-    region = "fra1"
-    ip_range = "10.222.100.0/24"
+    name     = "${var.workspace_prefix}-main-vm-tf-cloud"
+    region   = "fra1"
+    ip_range = "${var.workspace_ip_prefix}.100.0/24"
 }
 
 resource "digitalocean_droplet" "main" {
-    name = "main-piotrkoska"
-    image = "ubuntu-20-04-x64"
-    region = "fra1"
-    size = "s-1vcpu-1gb"    
+    name     = "${var.workspace_prefix}-main-vm"
+    image    = "${var.vm_size}"
+    region   = "fra1"
+    size     = "s-1vcpu-1gb"
     vpc_uuid = digitalocean_vpc.main.id
 }
 
 resource "digitalocean_droplet" "main2" {
-    name = "main-piotrkoska"
-    image = "ubuntu-20-04-x64"
-    region = "fra1"
-    size = "s-1vcpu-1gb"    
+    name     = "${var.workspace_prefix}-main-vm"
+    image    = "ubuntu-20-04-x64"
+    region   = "fra1"
+    size     = "s-1vcpu-1gb"
     vpc_uuid = digitalocean_vpc.main.id
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
-variable "token" {
-  description = "Digital Ocean token"
-  sensitive = true
+variable "do_token" {
+    description = "Digital Ocean token"
+    sensitive = true
+}
+
+variable "workspace_prefix" {
+    type        = string
+    description = "Workspace prefix"
+}
+
+variable "workspace_ip_prefix" {
+    type        = string
+    description = "Workspace IP prefix"
+}
+
+variable "vm_size" {
+    type        = string
+    description = "VM size"
 }
